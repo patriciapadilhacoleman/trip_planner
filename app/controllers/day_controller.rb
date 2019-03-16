410d12ff7ff4d6ccaa40bd40ece8d48e95b91fd3
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-03-15 19:20:16 -0700

    Deletes Daily Log

--- app/controllers/day_controller.rb
+++ app/controllers/day_controller.rb
@@ -5,9 +5,24 @@ class DayController < ApplicationController
     @day = Day.create(params[:day])
     @day.trip_id = params[:trip][:id]
     @day.save
-    binding.pry
     @traveler = current_user
     @trip = current_user.trips.find_by_id(params[:id])
     erb:"/trips/show"
   end
+
+  patch '/days/:id' do
+    @traveler = current_user
+    @day = Day.find_by_id(params[:id])
+    @day.update(params["day"])
+    erb:"trips/show"
+  end
+
+  delete '/days/:id' do
+    if day = Day.find_by_id(params[:id])
+      day.delete
+    end
+    @traveler = current_user
+    erb:"trips/show"
+  end
+
 end
