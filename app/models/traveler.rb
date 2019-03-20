1af7c56d38d82bc12a4b991d0415e81fd633d75d
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-03-20 11:27:29 -0700

    How to add Css styling files

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -1,7 +1,7 @@
 class Traveler < ActiveRecord::Base
 	has_secure_password
   validates :username, :presence => true
-	validates :first_name
+	validates :first_name, :presence =>true
   has_many :trips
 	has_many :tickets, through: :trips
 end
